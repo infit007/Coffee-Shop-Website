32134209240331dd8f24590e8f6dafc52375ae5b
infit007 <81976647+infit007@users.noreply.github.com>
2023-02-22 18:55:44 +0530

    added initial website files

---
+++ app.js
@@ -0,0 +1,100 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+const request = require("request");
+const https=require("https");
+
+const app=express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(req,res){
+  res.sendFile(__dirname+"/signup.html");
+})
+
+app.post("/", function(req,res){
+  const firstName=req.body.fname;
+  const lastName=req.body.fname;
+  const email=req.body.email;
+
+  var data={
+    members:[
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields:{
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+const jsonData = JSON.stringify(data);
+
+  const url="https://us9.api.mailchimp.com/3.0/lists/f5a691fb90";
+
+  const options = {
+    method:"POST",
+    auth: "infit1:16ccbf9e712957a26d14c087c082f7ea-us9"
+
+}
+  const request = https.request(url,options,function(response){
+      if(response.statusCode === 200){
+        res.sendFile(__dirname+"/success.html");
+      }else{
+        res.sendFile(__dirname+"/failure.html");
+      }
+
+        response.on("data",function(data){
+        console.log(JSON.parse(data));
+      })
+  })
+request.write(jsonData);
+request.end();
+});
+
+app.post("/failure",function(req,res){
+  res.redirect("/")
+})
+
+app.listen(process.env.PORT || 3000,function(){
+  console.log("Server is running on port 3000");
+})
+
+
+
+
+
+// app.use(bodyParser.urlencoded({extended:true}));
+//
+// app.get("/",function(req,res){
+//   res.sendFile(__dirname+"/index.html");
+// });
+//
+//api key
+//16ccbf9e712957a26d14c087c082f7ea-us9
+
+//list id
+//f5a691fb90
+
+
+
+// --data{
+//   "name": "$event_name",
+//   "contact": $footer_contact_info,
+//   "permission_reminder": "permission_reminder",
+//   "email_type_option": true,
+//   "campaign_defaults": $campaign_defaults
+// }
+
+// --data @- \
+// <<EOF | jq '.id'
+// {
+//   "name": "$event_name",
+//   "contact": $footer_contact_info,
+//   "permission_reminder": "permission_reminder",
+//   "email_type_option": true,
+//   "campaign_defaults": $campaign_defaults
+// }
+
